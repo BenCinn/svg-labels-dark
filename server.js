eae899bf09d2da7dbd4907944533e9cf6f3b2ed6
BenCinn <pp.rongpan@gmail.com>
2022-03-21 17:07:19 +0700

    Add a implementation in server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const exp = require("express");
 const app = exp();
 const makeLabel = require(".");
+const labelgen = require("./ghtests");
+const labelGen = require("./ghtests");
 
 app
   .get("/svg", (req, res) => {
@@ -9,7 +11,18 @@ app
   })
   .get("/", (req, res) => {
     res.sendFile(__dirname + "/docs/index.html");
+  })
+  .get("/tests", (req, res) => {
+    res.set("Content-Type", "image/svg+xml");
+    let username = req.query.user || "nodejs";
+    let repo = req.query.repo || "node";
+    labelGen({ user: username, repo: repo }).then(function (output) {
+      res.send(output);
+    });
   });
+/*labelgen({ user: "BenCinn", repo: "Node-Server" }).then(function (res) {
+  console.log(res);
+});*/
 var port = process.env.PORT || 3000;
 
 app.listen(port, () =>
