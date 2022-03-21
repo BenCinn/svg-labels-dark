25d63100d70631b6b51b64ecbc115443cd668dd8
BenCinn <pp.rongpan@gmail.com>
2022-03-21 18:55:14 +0700

    Add caching (rate-limit)

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const labelGen = require("./ghtests");
 app
   .get("/svg", (req, res) => {
     res.set("Content-Type", "image/svg+xml");
+    res.set("Cache-control", "public, max-age=7889400, s-maxage=7889400");
     res.send(makeLabel(req.query));
   })
   .get("/", (req, res) => {
@@ -14,6 +15,7 @@ app
   })
   .get("/tests", (req, res) => {
     res.set("Content-Type", "image/svg+xml");
+    res.set("Cache-control", "public, max-age=300, s-maxage=300");
     let username = req.query.user || "nodejs";
     let repo = req.query.repo || "node";
     labelGen({ user: username, repo: repo }).then(function (output) {
