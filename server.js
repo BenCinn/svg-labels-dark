c89ac3638b6f3ca6df9694c439bf3d74ff1f2eab
BenCinn <pp.rongpan@gmail.com>
2022-03-21 16:42:42 +0700

    Switch to express

--- server.js
+++ server.js
@@ -1,26 +1,17 @@
-const Koa = require('koa');
-const Router = require('@koa/router');
-const send = require('koa-send');
-const makeLabel = require('.');
-
-const app = new Koa();
-const router = new Router();
-require('koa-qs')(app);
-
-router
-  .get('/svg', ctx => {
-    ctx.response.type = 'image/svg+xml';
-    ctx.response.body = makeLabel(ctx.request.query);
-  })
-  .get('/', async (ctx) => {
-    await send(ctx, '/docs/index.html');
-  })
-  .get('(.*)', async (ctx) => {
-    await send(ctx, ctx.path, { root: __dirname + '/docs' });
-  });
-
-var port = process.env.PORT || 3000
+const exp = require("express");
+const app = exp();
+const makeLabel = require(".");
 
 app
-  .use(router.routes())
-  .listen(port, () => console.log('Koa is listening to http://localhost:' + port) );
+  .get("/svg", (req, res) => {
+    res.set("Content-Type", "image/svg+xml");
+    res.send(makeLabel(req.query));
+  })
+  .get("/", (req, res) => {
+    res.sendFile(__dirname + "/docs/index.html");
+  });
+var port = process.env.PORT || 3000;
+
+app.listen(port, () =>
+  console.log("Express is listening to http://localhost:" + port)
+);
